3015c467cd2137c43ff5acc1bcdb7adf5f461a86
Zandarh <ubaksxander@gmail.com>
2022-03-11 16:42:49 +0000

    Phwee

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,29 +7,21 @@
 
 void print_to_98(int n)
 {
-	if (n > 9)
+	if (n < 9)
 	{
-		while (n >= 98)
+		while (n < 98)
 		{
 			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d", n);
 			n--;
 		}
 	}
-	else
-	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-		}
-		if (n != 98)
-		{
-			printf(", ");
-		}
-		n++;
-	}
-	printf("\n");
+	printf("98\n");
 }
