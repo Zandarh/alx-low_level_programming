367868868189f03ea06247dc12157b9b2ffac5ba
Zandarh <ubaksxander@gmail.com>
2022-03-11 07:43:22 +0000

    Okay

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the letter of the alphabeth
+ * Description: print the letters of the alphabeth
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(0)
+{
+	int i = 87;
+
+	while (i < 113)
+	{
+		putchar(i)
+		i++
+	}
+	putchar(10);
+
+	return (0);
+}
