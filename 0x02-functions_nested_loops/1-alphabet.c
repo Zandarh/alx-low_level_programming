1934fc20a996b7e76a56892371e1bf36157f171c
Zandarh <ubaksxander@gmail.com>
2022-03-11 16:30:35 +0000

    Allow

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - This fuction prints print_alphabet
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(c + i);
+	}
+	return (0);
+}
