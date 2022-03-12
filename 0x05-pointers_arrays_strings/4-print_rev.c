1d83072b43b942cfd4a85860319f18e0ecf84204
Zandarh <ubaksxander@gmail.com>
2022-03-12 08:04:39 +0000

    Printing reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
  * print_rev - prints a string in revrse followed by a new line
- * @char *s: Declaration of a character *s
+ * @s: Declaration of a character *s
  * Return: No return
  */
 
@@ -10,11 +10,10 @@ void print_rev(char *s)
 {
 	int a;
 
-	for ( a = 0; s[a] != 0; a++)
+	for (a = 0; s[a] != 0; a++)
 	{
-	
 	}
-	for (a = a -1; a >= 0; a--)
+	for (a = a - 1; a >= 0; a--)
 	{
 		_putchar(s[a]);
 	}
