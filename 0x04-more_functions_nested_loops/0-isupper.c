db4cca8d40603d9a8d6106d39866beb990b36742
Zandarh <ubaksxander@gmail.com>
2022-03-12 05:26:03 +0000

    This is a start

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,9 +2,9 @@
 
 /**
  * _isupper - checks for uppercase
- * @c: character to checked for case
+ * @c: charater to checked for case
  *
- * Return: 1 if uppercase, 0 if not uppercase
+ * Returm: 1 if uppercase, 0 if not uppercase
  */
 
 int _isupper(int c)
