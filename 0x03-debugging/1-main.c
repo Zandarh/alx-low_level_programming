156bdd1aba05200e965078561c4883fbf4330963
Zandarh <ubaksxander@gmail.com>
2022-03-12 01:50:39 +0000

    Betty

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,23 +7,18 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-        i = 0;
+	i = 0;
 
-        while (i < 10) 
-        
-         /*while (i < 10)*/
-	/*{*/
-	/*	putchar(i);*/
-	/*}*/ 
-        {
-                putchar(i);
-        }
+	while (i < 10) // This is what is causing the infinity loop
+	{
+		putchar(i);
+	}
 
-        printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+	return (0);
 }
