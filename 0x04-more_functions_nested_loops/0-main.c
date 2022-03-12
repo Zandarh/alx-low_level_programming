b0f267962586581f8fe6d0b0c4bf8d574b3dab6c
Zandarh <ubaksxander@gmail.com>
2022-03-12 04:37:22 +0000

    game

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,8 +1,5 @@
 #include "main.h"
-
 #include <stdio.h>
-
-
 
 /**
  * main - check the code.
@@ -11,21 +8,12 @@
  */
 
 int main(void)
-
 {
+	char c;
 
-    char c;
-
-
-
-    c = 'A';
-
-    printf("%c: %d\n", c, _isupper(c));
-
-    c = 'a';
-
-    printf("%c: %d\n", c, _isupper(c));
-
-    return (0);
-
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
