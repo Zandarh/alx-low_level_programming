96176720f40f3feb9ec16a858ed279bd4e371446
Zandarh <ubaksxander@gmail.com>
2022-03-12 08:45:55 +0000

    Wawu

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every othr characteer of a string
+ * @str - Declartion of str
+ * Return: Void
+*/
+
+void puts2(char *str)
+{
+	int a;
+	char b;
+
+	for (a = 0; str[a] !=0; a++)
+	{
+		if (a % 2 == 0);
+		{
+			b = (str[a]);
+			_putchar(b);
+		}
+	}
+	_putchar('\n');
+}
