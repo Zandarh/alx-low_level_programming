7f800e2ef141bec2e12487642bd7f2931192523f
Zandarh <99367800+Zandarh@users.noreply.github.com>
2022-03-24 07:45:20 +0100

    srcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,26 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @Dest: This is the first srting
- * @src: This is the second string
- *
- * Return: Return Dest
- */
+* _strcat - concatenates two strings
+* @dest: pointer parameter for string
+* @src: pointer parameter for second string
+*
+* Return: concatenated string
+*/
 
 char *_strcat(char *dest, char *src)
 {
-	int a, b;
-	
-	a = 0;
+	int l, j;
 
-	for (a = 0; dest[a] != '\0'; ++a)
-	{
-	}
-	for (b = 0; src[b] != '\0'; ++b, ++a)
-	{
-		dest[a] =src[b];
-	}
-	dest[a] = '\0';
+	l = 0;
 
+	while (dest[l] != '\0')
+	{
+		++l;
+	}
+	for (j = 0; src[j] != '\0'; j++, ++l)
+	{
+		dest[l] = src[j];
+	}
 	return (dest);
 }
