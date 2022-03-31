d4437514ebd6eeb9f04a95c33eec6e2c46f49605
Zandarh <99367800+Zandarh@users.noreply.github.com>
2022-03-31 21:54:28 +0100

    deal

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
 /**
- * _strstr - locates a substring
- * @haystack: string checked
- * @needle: substring found
- *
- * Return: Always return a char
-**/
-
-char *_strstr(char *haystack, chr *needle)
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: first string.
+ * @accept: second string.
+ * Return: a pointer to the byte in s that matches one of the
+ * bytes in accept, or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	unsigned int i, j;
 
-	for (i = 0; needle[j]; j++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		if ((haystack[i] == needle[j]) || (haystack[i] >= needle[j]))
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			return (haystack);
+			if (*(s + i) == *(accept + j))
+				return (s + i);
 		}
-		else
-			return (needle);
 	}
+	return ('\0');
 }
