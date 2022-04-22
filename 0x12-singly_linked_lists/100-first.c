5ecb1dc330be0a86cf5f4de530877aafb6694d47
Zandarh <ubaksxander@gmail.com>
2022-04-22 06:45:19 +0000

    print

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * psalm - function executed before main
+ * bmain - function executed before main
  * Return: no return.
  */
 
-void __printer__ ((constructor)) psalm()
+void __attribute__ ((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
-}
