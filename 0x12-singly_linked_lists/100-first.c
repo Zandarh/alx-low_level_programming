566f140b0c9de3e60ecfd9b930d7596fbfd11ad4
Zandarh <ubaksxander@gmail.com>
2022-04-22 06:42:37 +0000

    print

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * psalm - function executed before main
+ * Return: no return.
+ */
+
+void __printer__ ((constructor)) psalm()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
