bcfa58e5bf51c075f7a8caaf1c99b12626b50fac
Zandarh <ubaksxander@gmail.com>
2022-10-06 01:17:00 +0100

    Task update

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * int linear_search - searches for a value in an array of
+ * linear_search - searches for a value in an array of
  * integers using the Linear search algorithm
  *
  * @array: pointer the first element of the array
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
 #ifndef _SEARCHING_ALGOS_H_
-#define _SEARCHING_ALGOS_H__
+#define _SEARCHING_ALGOS_H_
 
 #include <stdio.h>
 #include <stdlib.h>
